22bc6791bd00afeaffbc273db631c891c815221c
Thanakrit Sahachatchai <thanakrit.10197@gmail.com>
2020-12-01 00:32:59 +0700

    layout [result,player,bet]

--- src/App.js
+++ src/App.js
@@ -3,22 +3,251 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="bg-success">
+        <div className="container pt-3 h-100 d-flex justify-content-center">
+          <div className="row" style={{width: '90%'}}>
+            <div className="col-5">
+              <label className="text-white">Last Result BET KEYS</label>
+              <hr className="bg-secondary mb-3 mt-0 p-0" />
+              <div className="row m-0 p-0">
+                <div className="col-6 px-1">
+                  <label className="btn btn-block btn-sm p-1 bg-secondary text-white">sadasdsa</label>
+                </div>
+                <div className="col-6 px-1">
+                  <label className="btn btn-block btn-sm p-1 bg-secondary text-white">sadasdsa</label>
+                </div>
+                <div className="col-6 px-1">
+                  <label className="btn btn-block btn-sm p-1 bg-secondary text-white">sadasdsa</label>
+                </div>
+                <div className="col-6 px-1">
+                  <label className="btn btn-block btn-sm p-1 bg-secondary text-white">sadasdsa</label>
+                </div>
+                <div className="col-6 px-1">
+                  <label className="btn btn-block btn-sm p-1 bg-secondary text-white">sadasdsa</label>
+                </div>
+                <div className="col-6 px-1">
+                  <label className="btn btn-block btn-sm p-1 bg-secondary text-white">sadasdsa</label>
+                </div>
+                <div className="col-6 px-1">
+                  <label className="btn btn-block btn-sm p-1 bg-secondary text-white">sadasdsa</label>
+                </div>
+                <div className="col-6 px-1">
+                  <label className="btn btn-block btn-sm p-1 bg-secondary text-white">sadasdsa</label>
+                </div>
+                <div className="col-6 px-1">
+                  <label className="btn btn-block btn-sm p-1 bg-secondary text-white">sadasdsa</label>
+                </div>
+                <div className="col-6 px-1">
+                  <label className="btn btn-block btn-sm p-1 bg-secondary text-white">sadasdsa</label>
+                </div>
+                <div className="col-6 px-1">
+                  <label className="btn btn-block btn-sm p-1 bg-secondary text-white">sadasdsa</label>
+                </div>
+                <div className="col-6 px-1">
+                  <label className="btn btn-block btn-sm p-1 bg-secondary text-white">sadasdsa</label>
+                </div>
+                <div className="col-6 px-1">
+                  <label className="btn btn-block btn-sm p-1 bg-secondary text-white">sadasdsa</label>
+                </div>
+                <div className="col-6 px-1">
+                  <label className="btn btn-block btn-sm p-1 bg-secondary text-white">sadasdsa</label>
+                </div>
+              </div>
+            </div>
+            <div className="col-7" Style={{ maxHeight: '200px' }}>
+              <label className="text-white">Player BET KEYS</label>
+              <hr className="bg-secondary mb-3 mt-0 p-0" />
+              <div className="row m-0 p-0">
+                <div className="col-4 px-1">
+                  <label className="btn btn-block btn-sm p-1 bg-secondary text-white">sadasdsa</label>
+                </div>
+              </div>
+              <div className="row m-0 p-0">
+                <div className="col-4 px-1">
+                  <label className="btn btn-block btn-sm p-1 bg-secondary text-white">sadasdsa</label>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="bg-secondary">
+        <div className="container py-4">
+          <div className="row">
+            <div className="col-1 text-right">
+              <button className="btn btn-success mb-1" style={{ width: '74px', height: '232px' }}>0</button>
+            </div>
+            <div className="col-10 m-0 p-0">
+              <div className="row m-0 p-0">
+                <div className="col-4">
+                  <div className="row">
+                    <div className="col-3 m-0 p-0">
+                      <button className="btn btn-danger mb-1" style={{ width: '74px', height: '74px' }}>3</button>
+                    </div>
+                    <div className="col-3 m-0 p-0">
+                      <button className="btn btn-dark mb-1" style={{ width: '74px', height: '74px' }}>6</button>
+                    </div>
+                    <div className="col-3 m-0 p-0">
+                      <button className="btn btn-danger mb-1" style={{ width: '74px', height: '74px' }}>9</button>
+                    </div>
+                    <div className="col-3 m-0 p-0">
+                      <button className="btn btn-danger mb-1" style={{ width: '74px', height: '74px' }}>12</button>
+                    </div>
+                    <div className="col-3 m-0 p-0">
+                      <button className="btn btn-dark mb-1" style={{ width: '74px', height: '74px' }}>2</button>
+                    </div>
+                    <div className="col-3 m-0 p-0">
+                      <button className="btn btn-danger mb-1" style={{ width: '74px', height: '74px' }}>5</button>
+                    </div>
+                    <div className="col-3 m-0 p-0">
+                      <button className="btn btn-dark mb-1" style={{ width: '74px', height: '74px' }}>8</button>
+                    </div>
+                    <div className="col-3 m-0 p-0">
+                      <button className="btn btn-dark mb-1" style={{ width: '74px', height: '74px' }}>11</button>
+                    </div>
+                    <div className="col-3 m-0 p-0">
+                      <button className="btn btn-danger mb-1" style={{ width: '74px', height: '74px' }}>1</button>
+                    </div>
+                    <div className="col-3 m-0 p-0">
+                      <button className="btn btn-dark mb-1" style={{ width: '74px', height: '74px' }}>4</button>
+                    </div>
+                    <div className="col-3 m-0 p-0">
+                      <button className="btn btn-danger mb-1" style={{ width: '74px', height: '74px' }}>7</button>
+                    </div>
+                    <div className="col-3 m-0 p-0">
+                      <button className="btn btn-dark mb-1" style={{ width: '74px', height: '74px' }}>10</button>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-4">
+                  <div className="row">
+                    <div className="col-3 m-0 p-0">
+                      <button className="btn btn-dark mb-1" style={{ width: '74px', height: '74px' }}>15</button>
+                    </div>
+                    <div className="col-3 m-0 p-0">
+                      <button className="btn btn-danger mb-1" style={{ width: '74px', height: '74px' }}>18</button>
+                    </div>
+                    <div className="col-3 m-0 p-0">
+                      <button className="btn btn-danger mb-1" style={{ width: '74px', height: '74px' }}>21</button>
+                    </div>
+                    <div className="col-3 m-0 p-0">
+                      <button className="btn btn-dark mb-1" style={{ width: '74px', height: '74px' }}>24</button>
+                    </div>
+                    <div className="col-3 m-0 p-0">
+                      <button className="btn btn-danger mb-1" style={{ width: '74px', height: '74px' }}>14</button>
+                    </div>
+                    <div className="col-3 m-0 p-0">
+                      <button className="btn btn-dark mb-1" style={{ width: '74px', height: '74px' }}>17</button>
+                    </div>
+                    <div className="col-3 m-0 p-0">
+                      <button className="btn btn-dark mb-1" style={{ width: '74px', height: '74px' }}>20</button>
+                    </div>
+                    <div className="col-3 m-0 p-0">
+                      <button className="btn btn-danger mb-1" style={{ width: '74px', height: '74px' }}>23</button>
+                    </div>
+                    <div className="col-3 m-0 p-0">
+                      <button className="btn btn-dark mb-1" style={{ width: '74px', height: '74px' }}>13</button>
+                    </div>
+                    <div className="col-3 m-0 p-0">
+                      <button className="btn btn-danger mb-1" style={{ width: '74px', height: '74px' }}>16</button>
+                    </div>
+                    <div className="col-3 m-0 p-0">
+                      <button className="btn btn-danger mb-1" style={{ width: '74px', height: '74px' }}>19</button>
+                    </div>
+                    <div className="col-3 m-0 p-0">
+                      <button className="btn btn-dark mb-1" style={{ width: '74px', height: '74px' }}>22</button>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-4">
+                  <div className="row">
+                    <div className="col-3 m-0 p-0">
+                      <button className="btn btn-danger mb-1" style={{ width: '74px', height: '74px' }}>27</button>
+                    </div>
+                    <div className="col-3 m-0 p-0">
+                      <button className="btn btn-danger mb-1" style={{ width: '74px', height: '74px' }}>30</button>
+                    </div>
+                    <div className="col-3 m-0 p-0">
+                      <button className="btn btn-dark mb-1" style={{ width: '74px', height: '74px' }}>33</button>
+                    </div>
+                    <div className="col-3 m-0 p-0">
+                      <button className="btn btn-danger mb-1" style={{ width: '74px', height: '74px' }}>36</button>
+                    </div>
+                    <div className="col-3 m-0 p-0">
+                      <button className="btn btn-dark mb-1" style={{ width: '74px', height: '74px' }}>26</button>
+                    </div>
+                    <div className="col-3 m-0 p-0">
+                      <button className="btn btn-dark mb-1" style={{ width: '74px', height: '74px' }}>29</button>
+                    </div>
+                    <div className="col-3 m-0 p-0">
+                      <button className="btn btn-danger mb-1" style={{ width: '74px', height: '74px' }}>32</button>
+                    </div>
+                    <div className="col-3 m-0 p-0">
+                      <button className="btn btn-dark mb-1" style={{ width: '74px', height: '74px' }}>35</button>
+                    </div>
+                    <div className="col-3 m-0 p-0">
+                      <button className="btn btn-danger mb-1" style={{ width: '74px', height: '74px' }}>25</button>
+                    </div>
+                    <div className="col-3 m-0 p-0">
+                      <button className="btn btn-dark mb-1" style={{ width: '74px', height: '74px' }}>28</button>
+                    </div>
+                    <div className="col-3 m-0 p-0">
+                      <button className="btn btn-dark mb-1" style={{ width: '74px', height: '74px' }}>31</button>
+                    </div>
+                    <div className="col-3 m-0 p-0">
+                      <button className="btn btn-danger mb-1" style={{ width: '74px', height: '74px' }}>34</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="row m-0 p-0">
+                <div className="col-4 m-0 p-0">
+                  <button className="btn btn-success mb-1" style={{ height: '74px', width: '312px' }}>โซน 1-12</button>
+                </div>
+                <div className="col-4 m-0 p-0">
+                  <button className="btn btn-success mb-1" style={{ height: '74px', width: '312px' }}>โซน 13-24</button>
+                </div>
+                <div className="col-4 m-0 p-0">
+                  <button className="btn btn-success mb-1" style={{ height: '74px', width: '312px' }}>โซน 25-36</button>
+                </div>
+              </div>
+              <div className="row m-0 p-0">
+                <div className="col-2 m-0 p-0">
+                  <button className="btn btn-success mb-1" style={{ height: '74px', width: '154px' }}>ต่ำ 1-18</button>
+                </div>
+                <div className="col-2 m-0 p-0">
+                  <button className="btn btn-success mb-1" style={{ height: '74px', width: '154px' }}>คู่</button>
+                </div>
+                <div className="col-2 m-0 p-0">
+                  <button className="btn btn-danger mb-1" style={{ height: '74px', width: '154px' }}>แดง</button>
+                </div>
+                <div className="col-2 m-0 p-0">
+                  <button className="btn btn-dark mb-1" style={{ height: '74px', width: '154px' }}>ดำ</button>
+                </div>
+                <div className="col-2 m-0 p-0">
+                  <button className="btn btn-success mb-1" style={{ height: '74px', width: '154px' }}>สูง</button>
+                </div>
+                <div className="col-2 m-0 p-0">
+                  <button className="btn btn-success mb-1" style={{ height: '74px', width: '154px' }}>19-36</button>
+                </div>
+              </div>
+            </div>
+            <div className="col-1 text-left m-0 p-0">
+              <div className="row m-0 p-0">
+                <button className="btn btn-success mb-1" style={{ width: '74px', height: '74px' }}>2:1</button>
+              </div>
+              <div className="row m-0 p-0">
+                <button className="btn btn-success mb-1" style={{ width: '74px', height: '74px' }}>2:1</button>
+              </div>
+              <div className="row m-0 p-0">
+                <button className="btn btn-success mb-1" style={{ width: '74px', height: '74px' }}>2:1</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
