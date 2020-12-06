89f5c5433003c76fed7f8db6adb5fefaaadf6c2a
Thanakrit Sahachatchai <thanakrit.10197@gmail.com>
2020-12-06 12:35:02 +0700

    set disabled btn bet

--- src/component/pad_bet/index.js
+++ src/component/pad_bet/index.js
@@ -66,7 +66,9 @@ const Pad_Bet = ({ click_bet, status_clear }) => {
                 class: index === 5 || index === 14 || index === 23 || index === 32 ?
                     "border-red-800 bg-red-900"
                     :
-                    "border-gray-700 bg-gray-800"
+                    "border-gray-700 bg-gray-800",
+                icon: []
+
             }
             data_middle.push(data)
             index = index + 3
@@ -80,7 +82,9 @@ const Pad_Bet = ({ click_bet, status_clear }) => {
                 class: index === 1 || index === 7 || index === 16 || index === 19 || index === 25 || index === 34 ?
                     "border-red-800 bg-red-900"
                     :
-                    "border-gray-700 bg-gray-800"
+                    "border-gray-700 bg-gray-800",
+                icon: []
+
             }
             data_bottom.push(data)
             index = index + 3
@@ -156,11 +160,13 @@ const Pad_Bet = ({ click_bet, status_clear }) => {
                 even: false,
             })
         } else if (value_type === "STRAIGHTUP") {
-            // setInBet({
-            //     ...in_bet,
-            //     [`in_bet_${value_bet}`]: true,
-            // })
             data_top.filter((item) => (item.num === parseInt(value_bet))).map(data => {
+                data['icon'].push(true)
+            })
+            data_middle.filter((item) => (item.num === parseInt(value_bet))).map(data => {
+                data['icon'].push(true)
+            })
+            data_bottom.filter((item) => (item.num === parseInt(value_bet))).map(data => {
                 data['icon'].push(true)
             })
         }
@@ -179,70 +185,74 @@ const Pad_Bet = ({ click_bet, status_clear }) => {
                         <button key={i} disabled={item.icon.length === 2 ? true : false} className={"border-2 lg:w-20 lg:h-20 sm:w-16 sm:h-16 rounded-lg flex justify-center items-center " + item.class} id={item.num} name="STRAIGHTUP" onClick={(e) => { select_bet(e.target.name, e.target.id) }}>{item.num}
                             <label className="absolute grid grid-rows-1 grid-flow-col gap-0">
                                 {item.icon.map(icon =>
-                                    <img src={logo} className="lg:w-12 sm:w-8"/>
+                                    <img src={logo} className="lg:w-12 sm:w-8" />
                                 )}
                             </label>
                         </button>
                     )}
                     {data_middle.map((item, i) =>
-                        <button key={i} className={"border-2 lg:w-20 lg:h-20 sm:w-16 sm:h-16 rounded-lg flex justify-center items-center " + item.class} id={item.num} name="STRAIGHTUP" onClick={(e) => { select_bet(e.target.name, e.target.id) }}>{item.num}
-                            <div className="absolute" hidden={in_bet[`in_bet_${item.num}`] === true ? false : true}>
-                                <img src={logo} className="lg:w-14 sm:w-8" />
-                            </div>
+                        <button key={i} disabled={item.icon.length === 2 ? true : false} className={"border-2 lg:w-20 lg:h-20 sm:w-16 sm:h-16 rounded-lg flex justify-center items-center " + item.class} id={item.num} name="STRAIGHTUP" onClick={(e) => { select_bet(e.target.name, e.target.id) }}>{item.num}
+                            <label className="absolute grid grid-rows-1 grid-flow-col gap-0">
+                                {item.icon.map(icon =>
+                                    <img src={logo} className="lg:w-12 sm:w-8" />
+                                )}
+                            </label>
                         </button>
                     )}
                     {data_bottom.map((item, i) =>
-                        <button key={i} className={"border-2 lg:w-20 lg:h-20 sm:w-16 sm:h-16 rounded-lg flex justify-center items-center " + item.class} id={item.num} name="STRAIGHTUP" onClick={(e) => { select_bet(e.target.name, e.target.id) }}>{item.num}
-                            <div className="absolute" hidden={in_bet[`in_bet_${item.num}`] === true ? false : true}>
-                                <img src={logo} className="lg:w-14 sm:w-8" />
-                            </div>
+                        <button key={i} disabled={item.icon.length === 2 ? true : false} className={"border-2 lg:w-20 lg:h-20 sm:w-16 sm:h-16 rounded-lg flex justify-center items-center " + item.class} id={item.num} name="STRAIGHTUP" onClick={(e) => { select_bet(e.target.name, e.target.id) }}>{item.num}
+                            <label className="absolute grid grid-rows-1 grid-flow-col gap-0">
+                                {item.icon.map(icon =>
+                                    <img src={logo} className="lg:w-12 sm:w-8" />
+                                )}
+                            </label>
                         </button>
                     )}
                 </div>
                 <div className="grid grid-cols-3 mt-1 gap-1">
-                    <button className="border-2 border-green-700 bg-green-800 w-full lg:h-20 sm:h-16 rounded-lg flex justify-center items-center" name="DOZEN" id="1st" onClick={(e) => { select_bet(e.target.name, e.target.id) }}>โซน 1-12
+                    <button disabled={out_bet.dozen_1 === false ? true : false} className="border-2 border-green-700 bg-green-800 w-full lg:h-20 sm:h-16 rounded-lg flex justify-center items-center" name="DOZEN" id="1st" onClick={(e) => { select_bet(e.target.name, e.target.id) }}>โซน 1-12
                         <div className="absolute" hidden={out_bet.dozen_1}>
                             <img src={logo} className="lg:w-14 sm:w-8" />
                         </div>
                     </button>
-                    <button className="border-2 border-green-700 bg-green-800 w-full lg:h-20 sm:h-16 rounded-lg flex justify-center items-center" name="DOZEN" id="2nd" onClick={(e) => { select_bet(e.target.name, e.target.id) }}>โซน 13-24
+                    <button disabled={out_bet.dozen_2 === false ? true : false} className="border-2 border-green-700 bg-green-800 w-full lg:h-20 sm:h-16 rounded-lg flex justify-center items-center" name="DOZEN" id="2nd" onClick={(e) => { select_bet(e.target.name, e.target.id) }}>โซน 13-24
                         <div className="absolute" hidden={out_bet.dozen_2}>
                             <img src={logo} className="lg:w-14 sm:w-8" />
                         </div>
                     </button>
-                    <button className="border-2 border-green-700 bg-green-800 w-full lg:h-20 sm:h-16 rounded-lg flex justify-center items-center" name="DOZEN" id="3rd" onClick={(e) => { select_bet(e.target.name, e.target.id) }}>โซน 25-36
+                    <button disabled={out_bet.dozen_3 === false ? true : false} className="border-2 border-green-700 bg-green-800 w-full lg:h-20 sm:h-16 rounded-lg flex justify-center items-center" name="DOZEN" id="3rd" onClick={(e) => { select_bet(e.target.name, e.target.id) }}>โซน 25-36
                         <div className="absolute" hidden={out_bet.dozen_3}>
                             <img src={logo} className="lg:w-14 sm:w-8" />
                         </div>
                     </button>
                 </div>
                 <div className="grid grid-cols-6 mt-1 gap-1">
-                    <button className="border-2 border-green-700 bg-green-800 w-full lg:h-20 sm:h-16 rounded-lg flex justify-center items-center" name="HALF" id="SMALL" onClick={(e) => { select_bet(e.target.name, e.target.id) }}>ต่ำ 1-18
+                    <button disabled={out_bet.small === false ? true : false} className="border-2 border-green-700 bg-green-800 w-full lg:h-20 sm:h-16 rounded-lg flex justify-center items-center" name="HALF" id="SMALL" onClick={(e) => { select_bet(e.target.name, e.target.id) }}>ต่ำ 1-18
                         <div className="absolute" hidden={out_bet.small}>
                             <img src={logo} className="lg:w-14 sm:w-8" />
                         </div>
                     </button>
-                    <button className="border-2 border-green-700 bg-green-800 w-full lg:h-20 sm:h-16 rounded-lg flex justify-center items-center" name="HALF" id="EVEN" onClick={(e) => { select_bet(e.target.name, e.target.id) }}>คู่
+                    <button disabled={out_bet.even === false ? true : false} className="border-2 border-green-700 bg-green-800 w-full lg:h-20 sm:h-16 rounded-lg flex justify-center items-center" name="HALF" id="EVEN" onClick={(e) => { select_bet(e.target.name, e.target.id) }}>คู่
                         <div className="absolute" hidden={out_bet.even}>
                             <img src={logo} className="lg:w-14 sm:w-8" />
                         </div>
                     </button>
-                    <button className="border-2 border-red-800 bg-red-900 w-full lg:h-20 sm:h-16 rounded-lg flex justify-center items-center" name="HALF" id="RED" onClick={(e) => { select_bet(e.target.name, e.target.id) }}>แดง
+                    <button disabled={out_bet.red === false ? true : false} className="border-2 border-red-800 bg-red-900 w-full lg:h-20 sm:h-16 rounded-lg flex justify-center items-center" name="HALF" id="RED" onClick={(e) => { select_bet(e.target.name, e.target.id) }}>แดง
                         <div className="absolute" hidden={out_bet.red}>
                             <img src={logo} className="lg:w-14 sm:w-8" />
                         </div>
                     </button>
-                    <button className="border-2 border-gray-700 bg-gray-800 w-full lg:h-20 sm:h-16 rounded-lg flex justify-center items-center" name="HALF" id="BLACK" onClick={(e) => { select_bet(e.target.name, e.target.id) }}>ดำ
+                    <button disabled={out_bet.black === false ? true : false} className="border-2 border-gray-700 bg-gray-800 w-full lg:h-20 sm:h-16 rounded-lg flex justify-center items-center" name="HALF" id="BLACK" onClick={(e) => { select_bet(e.target.name, e.target.id) }}>ดำ
                         <div className="absolute" hidden={out_bet.black}>
                             <img src={logo} className="lg:w-14 sm:w-8" />
                         </div>
                     </button>
-                    <button className="border-2 border-green-700 bg-green-800 w-full lg:h-20 sm:h-16 rounded-lg flex justify-center items-center" name="HALF" id="HIGH" onClick={(e) => { select_bet(e.target.name, e.target.id) }}>สูง 19-36
+                    <button disabled={out_bet.high === false ? true : false} className="border-2 border-green-700 bg-green-800 w-full lg:h-20 sm:h-16 rounded-lg flex justify-center items-center" name="HALF" id="HIGH" onClick={(e) => { select_bet(e.target.name, e.target.id) }}>สูง 19-36
                         <div className="absolute" hidden={out_bet.high}>
                             <img src={logo} className="lg:w-14 sm:w-8" />
                         </div>
                     </button>
-                    <button className="border-2 border-green-700 bg-green-800 w-full lg:h-20 sm:h-16 rounded-lg flex justify-center items-center" name="HALF" id="OOD" onClick={(e) => { select_bet(e.target.name, e.target.id) }}>คี่
+                    <button disabled={out_bet.ood === false ? true : false} className="border-2 border-green-700 bg-green-800 w-full lg:h-20 sm:h-16 rounded-lg flex justify-center items-center" name="HALF" id="OOD" onClick={(e) => { select_bet(e.target.name, e.target.id) }}>คี่
                         <div className="absolute" hidden={out_bet.ood}>
                             <img src={logo} className="lg:w-14 sm:w-8" />
                         </div>
@@ -251,17 +261,17 @@ const Pad_Bet = ({ click_bet, status_clear }) => {
             </div>
             <div>
                 <div class="grid grid-rows-3 grid-flow-col gap-1">
-                    <button className="border-2 border-green-700 bg-green-800 lg:w-20 sm:w-16 sm:h-16 lg:h-20 sm:w-14 sm:h-14 rounded-lg flex justify-center items-center" name="COLUME" id="3rd" onClick={(e) => { select_bet(e.target.name, e.target.id) }}>2:1
+                    <button disabled={out_bet.colume_3 === false ? true : false} className="border-2 border-green-700 bg-green-800 lg:w-20 sm:w-16 sm:h-16 lg:h-20 sm:w-14 sm:h-14 rounded-lg flex justify-center items-center" name="COLUME" id="3rd" onClick={(e) => { select_bet(e.target.name, e.target.id) }}>2:1
                         <div className="absolute" hidden={out_bet.colume_3}>
                             <img src={logo} className="lg:w-14 sm:w-8" />
                         </div>
                     </button>
-                    <button className="border-2 border-green-700 bg-green-800 lg:w-20 sm:w-16 sm:h-16 lg:h-20 sm:w-14 sm:h-14 rounded-lg flex justify-center items-center" name="COLUME" id="2nd" onClick={(e) => { select_bet(e.target.name, e.target.id) }}>2:1
+                    <button disabled={out_bet.colume_2 === false ? true : false} className="border-2 border-green-700 bg-green-800 lg:w-20 sm:w-16 sm:h-16 lg:h-20 sm:w-14 sm:h-14 rounded-lg flex justify-center items-center" name="COLUME" id="2nd" onClick={(e) => { select_bet(e.target.name, e.target.id) }}>2:1
                         <div className="absolute" hidden={out_bet.colume_2}>
                             <img src={logo} className="lg:w-14 sm:w-8" />
                         </div>
                     </button>
-                    <button className="border-2 border-green-700 bg-green-800 lg:w-20 sm:w-16 sm:h-16 lg:h-20 sm:w-14 sm:h-14 rounded-lg flex justify-center items-center" name="COLUME" id="1st" onClick={(e) => { select_bet(e.target.name, e.target.id) }}>2:1
+                    <button disabled={out_bet.colume_1 === false ? true : false} className="border-2 border-green-700 bg-green-800 lg:w-20 sm:w-16 sm:h-16 lg:h-20 sm:w-14 sm:h-14 rounded-lg flex justify-center items-center" name="COLUME" id="1st" onClick={(e) => { select_bet(e.target.name, e.target.id) }}>2:1
                         <div className="absolute" hidden={out_bet.colume_1}>
                             <img src={logo} className="lg:w-14 sm:w-8" />
                         </div>
